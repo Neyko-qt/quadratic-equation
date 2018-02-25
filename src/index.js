79742415424d0bff3e8c765e5e9ca7099bb33a0c
Neyko-qt <fauster08@mail.ru>
2018-02-26 02:05:48 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  
+
+  var equationArray = equation.split(' ');
+
+  var a;
+  var b;
+  var c;
+  var solutions = [];
+  
+  
+  a = + equationArray[0];
+
+  if (equationArray[3] == '-')  {
+    b = (equationArray[4]) * (-1);
+  } else {
+    b = +equationArray[4];
+  }
+
+  if (equationArray[7] == '-')  {
+    c = (equationArray[8]) * (-1);
+  } else {
+    c = +equationArray[8];
+  }
+
+  var discriminant = b*b - 4*a*c;
+
+  if (discriminant >= 0) {
+    var x1 = Math.round(((-b) + Math.sqrt(discriminant)) / (2*a));
+    var x2 = Math.round(((-b) - Math.sqrt(discriminant)) / (2*a));
+  } else  {
+    return "Discriminant < 0 --> No roots"
+  }
+
+  
+  solutions.push(x1, x2);
+  return solutions.sort(function (a, b) {
+    return a - b;
+  });
 }
